1b53eeeee8f4f58b27962c9d228e447f64d1c3e1
Sven <Sven@koalashome.de>
2012-07-21 13:10:49 +0200

    Cache-to-Ramdisk-Funktion implementiert.

--- shell-Kram/ramdisk/copy_firefox.sh
+++ shell-Kram/ramdisk/copy_firefox.sh
@@ -11,10 +11,10 @@
 
 # Name der gepackten Firefox-Datei ohne ".tgz"
 # Nur diese Angabe muss bei jeder neuen Version angepasst werden.
-FIREFOXVERSION=firefox13
+FIREFOXVERSION="firefox13"
 
 # Pfad zur RAM-Disk
-RAM_DISK=/ramdisc
+RAM_DISK="/ramdisc"
 
 # Pfad zum Verzeichnis in dem sich Firefox befindet
 SRC_PFAD="$HOME/Programme"
@@ -23,9 +23,37 @@ SRC_PFAD="$HOME/Programme"
 #                 Einstellungen Ende
 #######################################################################
 
+NOQUIET=
 
 # Directory for saving data
 SYD_Dir=$RAM_DISK/$FIREFOXVERSION
+
+# Funktion zur Verzeichniserstellung
+# @see ram_cache_start.sh
+TempHome="${RAM_DISK}/${USER}"
+clean_and_link () 
+{
+  if [ ! $NOQUIET ]; then
+    V_AUSGABE="-v"
+  else
+    V_AUSGABE=""
+  fi
+  
+  NewDir="$1"
+  if [ -z "${NewDir}" ]; then
+    echo "Warning: args not found."
+    return
+  fi
+  if [ -e "${HOME}/${NewDir}" ]; then
+    rm -r -f $V_AUSGABE "${HOME}/${NewDir}"
+  fi
+  if [ -e "${TempHome}/_TMP_${NewDir}" ]; then
+    rm -r -f $V_AUSGABE "${TempHome}/_TMP_${NewDir}"
+  fi
+  mkdir $V_AUSGABE -p "${TempHome}/_TMP_${NewDir}" || return
+  ln -s -f "${TempHome}/_TMP_${NewDir}" "${NewDir}"
+}
+
 
 
 
@@ -51,6 +79,14 @@ else
   ($SYD_Dir/firefox &)
 fi
 
+# warte bis Firefox gestartet ist
+# TODO besser ist es den Prozess abzufragen?
+sleep 5
 
-
+# erstelle Cache-Verzeichnis in RAM_DISK und setze Link dorthin
+if [ -d .mozilla ]; then
+    for CacheDir in `find .mozilla -maxdepth 3 -name Cache`; do
+      clean_and_link "${CacheDir}"
+    done
+fi
 
